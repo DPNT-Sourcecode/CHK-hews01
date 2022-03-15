68c4ae67f8c9899af8c9635ceec04ae15aa9abbd
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 20:50:11 +0000

    Tue Mar 15 20:50:11 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,6 +7,8 @@ def test_sum_solution_adds_values():
 
 
 def test_sum_solution_accepts_0_to_100():
-    assert sum_solution(-1, 10)
+    with pytest.raises(Exception) as exc_info:
+        sum_solution(-1, 10)
+    
 
 

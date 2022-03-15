8e402c6d5d423528ae83a7dff106bcec327e6a2f
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 20:59:10 +0000

    Tue Mar 15 20:59:10 UTC 2022

--- errors/SUM_R1_errors.py
+++ errors/SUM_R1_errors.py
@@ -1,3 +1,5 @@
 class NotAnInteger(Exception):
     """Raised when a value is expected to be an integer and is not."""
-    pass
+    def __init__(self, variable):
+        self.message = ""
+        super().__init__(self.message)

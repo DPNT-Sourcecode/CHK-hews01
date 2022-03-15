cfe0c9930a97db063a01dba89c9053cc27688658
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 22:23:10 +0000

    Tue Mar 15 22:23:10 UTC 2022

---
+++ errors/HLO_R1_errors.py
@@ -0,0 +1,5 @@
+class NotAnr(Exception):
+    """Raised when a value is expected to be an integer but is not."""
+    def __init__(self, variable):
+        self.message = "{} is not an integer.".format(variable)
+        super().__init__(self.message)
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -12,10 +12,12 @@ def does_not_raise():
     'input, expected_output',
     [
         ("Grace", "Hello Grace"),
+        ("Tom", "Hello Tom"),
     ]
 )
 def test_hello_solution(input, expected_output):
     assert hello(input) == expected_output
+
 
 
 

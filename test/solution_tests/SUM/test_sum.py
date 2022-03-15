2482b8cbc0fb9adde9d6bf1551d84539ae38d558
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 21:35:50 +0000

    Tue Mar 15 21:35:50 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,12 @@
 from solutions.SUM import sum_solution
 from errors.SUM_R1_errors import NotAnInteger, OutOfRange
 import pytest
+from contextlib import contextmanager
+
+
+@contextmanager
+def does_not_raise():
+    yield
 
 
 def test_sum_solution_adds_values():
@@ -10,6 +16,7 @@ def test_sum_solution_adds_values():
 @pytest.mark.parametrize(
     'value_1, value_2, expected_error, expected_error_message',
     [
+        (1, 2, does_not_raise(), ''),
         ('a', 10, pytest.raises(NotAnInteger), 'a is not an integer.'),
         ([], 10, pytest.raises(NotAnInteger), '[] is not an integer.'),
         ({}, 10, pytest.raises(NotAnInteger), '{} is not an integer.'),
@@ -17,7 +24,7 @@ def test_sum_solution_adds_values():
         ((2, 3, 4), 1000.3, pytest.raises(NotAnInteger), '(2, 3, 4) is not an integer.')
     ]
 )
-def test_sum_solution_non_integers_exception(value_1, value_2, expected_error, expected_error_message):
+def test_sum_solution_only_accepts_integers(value_1, value_2, expected_error, expected_error_message):
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
     assert exc_info.value.message == expected_error_message
@@ -28,11 +35,15 @@ def test_sum_solution_non_integers_exception(value_1, value_2, expected_error, e
     [
         (-1, 2, pytest.raises(OutOfRange), '-1 is not between 1 and 100.'),
         (1, 200, pytest.raises(OutOfRange), '200 is not between 1 and 100.'),
+        (1000, 5, pytest.raises(OutOfRange), '1000 is not between 1 and 100.'),
+        (-1000, 9999999999999, pytest.raises(OutOfRange), '-1000 is not between 1 and 100.'),
+        (100, 0, pytest.raises(OutOfRange), '0 is not between 1 and 100.'),
     ]
 )
 def test_sum_solution_out_of_range_exception(value_1, value_2, expected_error, expected_error_message):
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
     assert exc_info.value.message == expected_error_message
+
 
 

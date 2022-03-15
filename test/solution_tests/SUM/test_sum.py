5313726876b17a6cac1003bea3613acdbeb1b9b4
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 21:20:50 +0000

    Tue Mar 15 21:20:50 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -10,15 +10,16 @@ def test_sum_solution_adds_values():
 @pytest.mark.parametrize(
     'value_1, value_2, expected_error, expected_error_message',
     [
-        ('a', 10, pytest.raises(NotAnInteger)),
-        ([], 10, pytest.raises(NotAnInteger)),
-        ({}, 10, pytest.raises(NotAnInteger))
+        ('a', 10, pytest.raises(NotAnInteger), 'a is not an integer.'),
+        ([], 10, pytest.raises(NotAnInteger), '[] is not an integer.'),
+        ({}, 10, pytest.raises(NotAnInteger), '{} is not an integer.'),
+        (3, 1000.3, pytest.raises(NotAnInteger), '{} is not an integer.')
     ]
 )
 def test_sum_solution_rejects_non_integers(value_1, value_2, expected_error, expected_error_message):
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
-    assert exc_info == expected_error_message
-    
+    assert exc_info.value.message == expected_error_message
+
 
 

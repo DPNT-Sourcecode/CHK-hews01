a16cd0fb34b01db45ee8af870e94b2783caebdde
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 21:17:50 +0000

    Tue Mar 15 21:17:50 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -8,13 +8,17 @@ def test_sum_solution_adds_values():
 
 
 @pytest.mark.parametrize(
-    'value_1, value_2, expected_error',
+    'value_1, value_2, expected_error, expected_error_message',
     [
-        pytest.param('a', 10, pytest.raises(NotAnInteger), id="string and integer")
+        ('a', 10, pytest.raises(NotAnInteger)),
+        ([], 10, pytest.raises(NotAnInteger)),
+        ({}, 10, pytest.raises(NotAnInteger))
     ]
 )
-def test_sum_solution_rejects_non_integers(value_1, value_2, expected_error):
-    with expected_error:
+def test_sum_solution_rejects_non_integers(value_1, value_2, expected_error, expected_error_message):
+    with expected_error as exc_info:
         sum_solution(value_1, value_2)
+    assert exc_info == expected_error_message
+    
 
 

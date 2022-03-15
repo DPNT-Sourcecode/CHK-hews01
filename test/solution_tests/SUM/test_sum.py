6eeb81da5cfde505f6922b45020096b0c2ca21ac
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 21:23:50 +0000

    Tue Mar 15 21:23:50 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -13,13 +13,15 @@ def test_sum_solution_adds_values():
         ('a', 10, pytest.raises(NotAnInteger), 'a is not an integer.'),
         ([], 10, pytest.raises(NotAnInteger), '[] is not an integer.'),
         ({}, 10, pytest.raises(NotAnInteger), '{} is not an integer.'),
-        (3, 1000.3, pytest.raises(NotAnInteger), '{} is not an integer.')
+        (3, 1000.3, pytest.raises(NotAnInteger), '1000.3 is not an integer.'),
+        ((2, 3, 4), 1000.3, pytest.raises(NotAnInteger), '(2, 3, 4) is not an integer.')
     ]
 )
 def test_sum_solution_rejects_non_integers(value_1, value_2, expected_error, expected_error_message):
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
     assert exc_info.value.message == expected_error_message
+
 
 
 

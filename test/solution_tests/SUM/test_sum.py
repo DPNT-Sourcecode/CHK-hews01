dbb6fd4a4ca8689d368e8b71f87249457c93cb83
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 21:39:38 +0000

    Tue Mar 15 21:39:38 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -17,6 +17,7 @@ def test_sum_solution_adds_values():
     'value_1, value_2, expected_error, expected_error_message',
     [
         (1, 2, does_not_raise(), ''),
+        (21, 2, does_not_raise(), ''),
         ('a', 10, pytest.raises(NotAnInteger), 'a is not an integer.'),
         ([], 10, pytest.raises(NotAnInteger), '[] is not an integer.'),
         ({}, 10, pytest.raises(NotAnInteger), '{} is not an integer.'),
@@ -27,7 +28,8 @@ def test_sum_solution_adds_values():
 def test_sum_solution_only_accepts_integers(value_1, value_2, expected_error, expected_error_message):
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
-    assert exc_info.value.message == expected_error_message
+    if exc_info or expected_error_message:
+        assert exc_info.value.message == expected_error_message
 
 
 @pytest.mark.parametrize(
@@ -44,6 +46,7 @@ def test_sum_solution_out_of_range_exception(value_1, value_2, expected_error, e
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
     assert exc_info.value.message == expected_error_message
+
 
 
 

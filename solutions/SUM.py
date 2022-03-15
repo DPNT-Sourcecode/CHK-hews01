c27ead6a2edcb5b744872337e2e8890557da40bd
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 21:29:50 +0000

    Tue Mar 15 21:29:50 UTC 2022

--- solutions/SUM.py
+++ solutions/SUM.py
@@ -1,12 +1,17 @@
-from errors.SUM_R1_errors import NotAnInteger
+from errors.SUM_R1_errors import NotAnInteger, OutOfRange
 
 
 def sum_solution(int_1, int_2):
-    if not isinstance(int_1, int):
-        raise NotAnInteger(int_1)
-    if not isinstance(int_2, int):
-        raise NotAnInteger(int_2)
+
+    int_list = [int_1, int_2]
+
+    for integer in int_list:
+        if not isinstance(integer, int):
+            raise NotAnInteger(integer)
+        if not 1 >= integer >= 100:
+            raise OutOfRange
 
     int_sum = int_1 + int_2
     return int_sum
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,5 +1,5 @@
 from solutions.SUM import sum_solution
-from errors.SUM_R1_errors import NotAnInteger
+from errors.SUM_R1_errors import NotAnInteger, OutOfRange
 import pytest
 
 
@@ -17,7 +17,20 @@ def test_sum_solution_adds_values():
         ((2, 3, 4), 1000.3, pytest.raises(NotAnInteger), '(2, 3, 4) is not an integer.')
     ]
 )
-def test_sum_solution_rejects_non_integers(value_1, value_2, expected_error, expected_error_message):
+def test_sum_solution_non_integers_exception(value_1, value_2, expected_error, expected_error_message):
     with expected_error as exc_info:
         sum_solution(value_1, value_2)
     assert exc_info.value.message == expected_error_message
+
+
+@pytest.mark.parametrize(
+    'value_1, value_2, expected_error, expected_error_message',
+    [
+        (-1, 2, pytest.raises(OutOfRange), '-1 is not between 1 and 100.'),
+    ]
+)
+def test_sum_solution_out_of_range_exception(value_1, value_2, expected_error, expected_error_message):
+    with expected_error as exc_info:
+        sum_solution(value_1, value_2)
+    assert exc_info.value.message == expected_error_message
+

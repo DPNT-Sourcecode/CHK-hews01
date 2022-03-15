630ff5f5c14da1171f9f527277006cc03fe36a29
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 22:12:58 +0000

    Tue Mar 15 22:12:58 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -5,16 +5,17 @@ from errors.SUM_R1_errors import NotAnInteger, OutOfRange
 def compute(int_1, int_2):
 
     int_list = [int_1, int_2]
-    lower_limit = 1
+    lower_limit = 0
     upper_limit = 100
 
     for integer in int_list:
         if not isinstance(integer, int):
-            raise Exception
+            raise NotAnInteger(integer)
         if not lower_limit <= integer <= upper_limit:
-            raise Exception
+            raise OutOfRange(integer, lower_limit, upper_limit)
 
     int_sum = int_1 + int_2
     return int_sum
+
 
 

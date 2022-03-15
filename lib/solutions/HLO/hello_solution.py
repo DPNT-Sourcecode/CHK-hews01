0535949c44e9da85002451f2691761b3121aa197
sbx_user1051 <sbx_user1051@169.254.2.13>
2022-03-15 22:30:48 +0000

    Tue Mar 15 22:30:48 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,10 +2,13 @@ from errors.HLO_R1_errors import NotAString
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
+
+
 def hello(friend_name):
     if not isinstance(friend_name, str):
         raise NotAString(friend_name)
     return "Hello {}".format(friend_name)
+
 
 
 

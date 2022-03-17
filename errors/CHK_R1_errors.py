e0db022f4649e4665fd0d32e03922138ed7f7161
sbx_user1051 <sbx_user1051@169.254.159.197>
2022-03-17 19:50:47 +0000

    Thu Mar 17 19:50:47 UTC 2022

--- errors/CHK_R1_errors.py
+++ errors/CHK_R1_errors.py
@@ -4,9 +4,3 @@ class NotAnInteger(Exception):
         self.message = "{} is not an integer.".format(variable)
         super().__init__(self.message)
 
-
-class OutOfRange(Exception):
-    """Raised when a value is outside a permitted range."""
-    def __init__(self, variable, lower_limit, upper_limit):
-        self.message = "{} is not between {} and {}.".format(variable, lower_limit, upper_limit)
-        super().__init__(self.message)

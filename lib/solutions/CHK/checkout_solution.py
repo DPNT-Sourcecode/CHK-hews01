408c91175b331df6f14dcdb672ced684e0658b88
sbx_user1051 <sbx_user1051@169.254.159.197>
2022-03-17 20:08:47 +0000

    Thu Mar 17 20:08:47 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,16 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    price_table = {
+        "A": {"Price": 50, "Special Offers": {3: 130}},
+        "B": {"Price": 30, "Special Offers": {2: 45}},
+        "C": {"Price": 20},
+        "D": {"Price": 15}
+    }
+
+    sku_row = price_table.get(skus)
+    if sku_row:
+        price = sku_row["Price"]
+        return price
+
+

690f580e818aa399a7318e46dfd28f19220ef46e
sbx_user1051 <sbx_user1051@169.254.159.197>
2022-03-17 21:52:17 +0000

    Thu Mar 17 21:52:17 UTC 2022

--- test/solution_tests/CHK/test_check.py
+++ test/solution_tests/CHK/test_check.py
@@ -38,21 +38,17 @@ def does_not_raise():
         ('X', 90),
         ('Y', 10),
         ('Z', 50),
+        ('s', -1),
+        (34234, -1),
     ]
 )
 def test_checkout_solution(input_value, expected_value):
     assert checkout(input_value) == expected_value
 
 
-
 @pytest.mark.parametrize(
     'input_value,expected_value',
     [
-        ('A', 50),
-        ('B', 30),
-        ('C', 20),
-        ('D', 15),
-        ('E', 40),
         ('AA', 100),
         ('ABCD', 115),
         ('AAA', 130),
@@ -75,31 +71,29 @@ def test_checkout_solution(input_value, expected_value):
 )
 def test_checkout_solution_multibuy(input_value, expected_value):
     assert checkout(input_value) == expected_value
-"""@pytest.mark.parametrize(
-    'input_value, expected_error, expected_error_message',
-    [
-        (123, pytest.raises(NotAString), "123 is not a string."),
-        (['grace'], pytest.raises(NotAString), "['grace'] is not a string."),
-        ({'a': 5}, pytest.raises(NotAString), "{'a': 5} is not a string."),
-    ]
-)
-def test_checkout_only_accepts_strings(input_value, expected_error, expected_error_message):
-    with expected_error as exc_info:
-        checkout(input_value)
-    if exc_info or expected_error_message:
-        assert exc_info.value.message == expected_error_message
 
 
 @pytest.mark.parametrize(
-    'input_value, expected_error, expected_error_message',
+    'input_value,expected_value',
     [
-        ("F", pytest.raises(NotInPriceTable), "F is not an SKU in the price table."),
-        ("j75", pytest.raises(NotInPriceTable), "j75 is not an SKU in the price table."),
+        ('AA', 100),
+        ('ABCD', 115),
+        ('AAA', 130),
+        ('BB', 45),
+        ('AAAAA', 200),
+        ('AAAAAA', 250),
+        ('AAAAAAAAA', 380),
+        ('BBBBB', 120),
+
+        ('EEB', 80),
+        ('EEEB', 120),
+        ('EEEEBB', 160),
+        ('F', 10),
+        ('FF', 20),
+        ('FFF', 20),
+        ('FFFF', 30),
+        ('FFFFFF', 40)
     ]
 )
-def test_checkout_only_accepts_known_skus(input_value, expected_error, expected_error_message):
-    with expected_error as exc_info:
-        checkout(input_value)
-    if exc_info or expected_error_message:
-        assert exc_info.value.message == expected_error_message
-"""
+def test_checkout_solution_free_item(input_value, expected_value):
+    assert checkout(input_value) == expected_value

9c11f415012235b48ee095614182eaa9b87e1dba
sbx_user1051 <sbx_user1051@169.254.159.197>
2022-03-17 19:47:47 +0000

    Thu Mar 17 19:47:47 UTC 2022

---
+++ errors/CHK_R1_errors.py
@@ -0,0 +1,12 @@
+class NotAnInteger(Exception):
+    """Raised when a value is expected to be an integer but is not."""
+    def __init__(self, variable):
+        self.message = "{} is not an integer.".format(variable)
+        super().__init__(self.message)
+
+
+class OutOfRange(Exception):
+    """Raised when a value is outside a permitted range."""
+    def __init__(self, variable, lower_limit, upper_limit):
+        self.message = "{} is not between {} and {}.".format(variable, lower_limit, upper_limit)
+        super().__init__(self.message)

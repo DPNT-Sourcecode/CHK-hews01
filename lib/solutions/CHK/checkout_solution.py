6729169e15548a8cd583813cd2e259045de45a79
sbx_user1051 <sbx_user1051@169.254.159.197>
2022-03-17 20:51:08 +0000

    Thu Mar 17 20:51:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,8 +8,8 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     price_table = {
-        "A": {"Price": 50, "Special Offers": {'Units': 3, "Price": 130}},
-        "B": {"Price": 30, "Special Offers": {'Units': 2, "Price": 45}},
+        "A": {"Price": 50, "Special Offers": [{'Units': 3, "Price": 130}, {'Units': 5, "Price": 200}]},
+        "B": {"Price": 30, "Special Offers": [{'Units': 2, "Price": 45}]},
         "C": {"Price": 20},
         "D": {"Price": 15},
         "E": {"Price": 40}
@@ -21,22 +21,21 @@ def checkout(skus):
         if sku_row:
             if sku not in sku_collector.keys():
                 sku_collector[sku] = 0
-
             sku_collector[sku] += 1
-            sku_count = sku_collector[sku]
-
-            price = sku_row["Price"]
-            total_price += price
-
-            sku_offers = sku_row.get('Special Offers')
-            if sku_offers:
-                sku_offer_units = sku_offers.get('Units')
-                sku_offer_price = sku_offers.get('Price')
-                if sku_count % sku_offer_units == 0:
-                    total_price -= price * sku_offer_units
-                    total_price += sku_offer_price
         else:
             return -1
+    total_price = 0
+    for sku in sku_collector.keys():
+        sku_count = sku_collector[sku]
+        unit_price = sku_row["Price"]
+        total_price += unit_price * sku_count
+
+        sku_offers = sku_row.get('Special Offers')
+
+        #if sku_offers:
+            #sku_offer_units = sku_offers.get('Units')
+            #sku_offer_price = sku_offers.get('Price')
 
     return total_price
+
 

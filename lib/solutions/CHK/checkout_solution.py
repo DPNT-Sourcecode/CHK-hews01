b17c81375d15477784de8482b9617609d89faa18
sbx_user1051 <sbx_user1051@169.254.159.197>
2022-03-17 20:26:47 +0000

    Thu Mar 17 20:26:47 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,20 +9,33 @@ def checkout(skus):
         raise NotAString(skus)
 
     price_table = {
-        "A": {"Price": 50, "Special Offers": {3: 130}},
-        "B": {"Price": 30, "Special Offers": {2: 45}},
+        "A": {"Price": 50, "Special Offers": {3: 30}},
+        "B": {"Price": 30, "Special Offers": {2: 15}},
         "C": {"Price": 20},
         "D": {"Price": 15}
     }
     total_price = 0
+    sku_collector = {}
     for sku in skus:
         sku_row = price_table.get(sku)
         if sku_row:
+            if sku not in sku_collector.keys():
+                sku_collector[sku] = 0
+
+            sku_collector[sku] += 1
             price = sku_row["Price"]
+
+            sku_offer = sku_row.get('Special Offers')
+            if sku_offer:
+                if sku_collector[sku] 
+
+
+
             total_price += price
         else:
             raise NotInPriceTable(skus)
 
     return total_price
+
 
 
